bd23f9c2da47b9745b1f3145ea83b3580c688c70
jdanyescobar-boop <jdany.escobar@gmail.com>
2026-02-04 21:46:57 -0600

    Se implementa optimizacion git status

--- composeApp/src/commonMain/kotlin/com/example/contraap/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/contraap/App.kt
@@ -1,5 +1,6 @@
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.example.contraap.ui.screens.RegisterScreen
 
 @Composable
 fun App() {
---
+++ composeApp/src/commonMain/kotlin/com/example/contraap/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.contraap.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class RegisterViewModel {
+    var nombre by mutableStateOf("")
+    var correo by mutableStateOf("")
+    var telefono by mutableStateOf("")
+    var password by mutableStateOf("")
+    var especialidad by mutableStateOf("")
+    var mostrarDialogo by mutableStateOf(false)
+
+    fun onRegister() {
+        // Aquí validas y guardas
+        mostrarDialogo = true
+    }
+}
